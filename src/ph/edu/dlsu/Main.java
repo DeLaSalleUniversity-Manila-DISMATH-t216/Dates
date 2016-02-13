6668890d9b41ee74c8b1c674d3671f52553c8591
aenoncunanan <aenon_cunanan2@yahoo.com>
2016-02-13 17:06:14 +0800

    Initial commit

---
+++ src/ph/edu/dlsu/Main.java
@@ -0,0 +1,22 @@
+package ph.edu.dlsu;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Date d = new Date();
+        System.out.println("Today is: " +d);
+
+        GregorianCalendar gc = new GregorianCalendar(2016, 1, 13);
+        gc.add(GregorianCalendar.DATE, 1);
+        Date d2 = gc.getTime();
+
+        DateFormat df = DateFormat.getDateInstance(DateFormat.FULL);
+        String sd = df.format(d2);
+        System.out.println(sd);
+
+    }
+}
